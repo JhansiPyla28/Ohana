8bf89ffcfd92ad911078f93e489b7cec9102bb71
ravi_varma <ravivarmarv99@gmail.com>
2024-08-04 12:13:55 +0530

    image replacing

--- src/components/home/About.js
+++ src/components/home/About.js
@@ -41,7 +41,7 @@ export default function About() {
                   <img
                     className="img-fluid rounded w-75 wow zoomIn"
                     data-wow-delay="0.1s"
-                    src="/assets/img/about-2.jpg"
+                    src="/assets/img/about-2.jpeg"
                     style={{ marginTop: "25%" }}
                   />
                 </div>
